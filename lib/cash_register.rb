7ad36fc9cc76b9fb8662b9fd3e11399c3076b53b
Daksha Patel <dakshapatel100@gmail.com>
2018-05-12 23:55:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ def add_item(item, price, quantity =1)
 end
 
 def apply_discount
-  if @discount == 0
+  if @discount == 20
     return "There is no discount to apply."
   else
   @total -= @total * @discount /100
