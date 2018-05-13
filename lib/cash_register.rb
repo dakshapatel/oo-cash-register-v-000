0b1941865337ead3820cb6677f730d73deff23f4
Daksha Patel <dakshapatel100@gmail.com>
2018-05-13 01:24:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,8 +17,8 @@ def add_item(item, price, quantity =1)
   #binding.pry
  self.total += price * quantity
  quantity.times do
- @all_items << item
-end
+   @all_items << item
+ end
   self.last_transaction = price * quantity
 end
 
