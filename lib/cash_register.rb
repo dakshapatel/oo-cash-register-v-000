7ba3d0bc26eb51d278697e02c793de2affe349cc
Daksha Patel <dakshapatel100@gmail.com>
2018-05-13 00:33:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,12 +20,13 @@ def add_item(item, price, quantity =1)
 end
 
 def apply_discount
-  if self.total = @total * @discount /100
-  return "After the discount, the total comes to $#{self.total}."
-  else 
-    return "There is no discount to apply."
-  end
+  if @discount == 0
+  return "There is no discount to apply."
+elsif 
+  
+  "After the discount, the total comes to $#{self.total}."
 
+end 
 
 end
 
