adfce8086c0e1c7b2867d7c88dc127ec74a7ea10
Daksha Patel <dakshapatel100@gmail.com>
2018-05-13 00:08:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ def apply_discount
   if @discount == 0
     return "There is no discount to apply."
   else
-  @total -= @total * @discount /100.0
+  @total -= @total * @discount /100
   return "After the discount, the total comes to $#{@total}."
   end
 end
